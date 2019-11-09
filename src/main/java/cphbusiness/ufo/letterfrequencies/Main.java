101b4a6d7246ff7e96621e3dcea0932a85eb481f
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-11-10 00:56:09 +0100

    Update Main.java

--- src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
+++ src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
@@ -9,7 +9,13 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import static java.util.stream.Collectors.toMap;
-import org.springframework.util.StopWatch;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.StandardOpenOption;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Frequency analysis Inspired by
@@ -17,19 +23,59 @@ import org.springframework.util.StopWatch;
  *
  * @author kasper
  */
-public class Main {
 
+public class Main {
+    
+private static String fileName = "C:\\Users\\Jonas\\Documents\\GitHub\\Code-Performance_letter-frequencies\\src\\main\\resources\\FoundationSeries.txt";
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        StopWatch stopwatch = new StopWatch();
-        stopwatch.start();
-        String fileName = "C:\\Users\\Jonas\\Documents\\GitHub\\Code-Performance_letter-frequencies\\src\\main\\resources\\FoundationSeries.txt";
+        Instant starts = Instant.now();
+         long startTime = System.currentTimeMillis();
         Reader reader = new FileReader(fileName);
+        Scanner sc = new Scanner(fileName);
+        BufferedReader bufferedReader = new BufferedReader(reader);
         Map<Integer, Long> freq = new HashMap<>();
-        tallyChars(reader, freq);
-        print_tally(freq);
+        //tallyChars(reader, freq);
+        //tallyCharsOptimizedWithScanner(sc, freq);
+        tallyCharsOptimizedWithBufferedReader(bufferedReader, freq);
+        //print_tally(freq);
+         Instant ends = Instant.now();
+            System.out.println("Duration for main method: "+Duration.between(starts, ends));
+    }
+    
+   
+    private static void tallyCharsOptimizedWithScanner(Scanner sc, Map<Integer, Long> freq) throws IOException {
+        Instant starts = Instant.now();
+        int t = 0;
+        try {
+        while (sc.hasNextLine()) {
+            freq.put(t, freq.get(t) + 1);
+        }
+        sc.close();
+    } 
+    catch (NullPointerException np) {
+                freq.put(t, 1L);
+            };
+            
+            Instant ends = Instant.now();
+            System.out.println("Duration: "+Duration.between(starts, ends));
+    }
+    
+    private static void tallyCharsOptimizedWithBufferedReader(BufferedReader bufferedReader, Map<Integer, Long> freq) throws IOException {
+        String strCurrentLine;   
+        int t = 0;
+        try {
+        while ((strCurrentLine = bufferedReader.readLine()) != null) {
+
+         freq.put(t, freq.get(t) + 1);
+        }
+
+       } catch (NullPointerException np) {
+                freq.put(t, 1L);
+            };
     }
 
     private static void tallyChars(Reader reader, Map<Integer, Long> freq) throws IOException {
+        Instant starts = Instant.now();
         int b;
         while ((b = reader.read()) != -1) {
             try {
@@ -38,6 +84,8 @@ public class Main {
                 freq.put(b, 1L);
             };
         }
+        Instant ends = Instant.now();
+            System.out.println("Duration: "+Duration.between(starts, ends));
     }
 
     private static void print_tally(Map<Integer, Long> freq) {
